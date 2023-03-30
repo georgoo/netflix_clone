5cc822ac5627f0d5169b0f3538b62d9c9573f33f
George Paul <georgepaulkp@gmail.com>
2023-03-30 16:44:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import { action,originals } from "./urls";
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title="Netflix Originals"/>
+      <RowPost url={action} title="Action" isSmall/>
     </div>
   );
 }
---
+++ src/constants/constant.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '5498c5d31250943a55508251c24b0de1'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
